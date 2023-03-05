70d9b410143c10bed0f850aa050dd2dd15ea9c67
drunken-hearted <114334493+drunken-hearted@users.noreply.github.com>
2023-03-05 19:02:00 +1100

    added sound

--- lua/entities/heart_turbolaser/init.lua
+++ lua/entities/heart_turbolaser/init.lua
@@ -4,11 +4,11 @@ include("shared.lua");
 
 function ENT:Initialize()
 	self:SetModel("models/props_phx/construct/wood/wood_boardx4.mdl");
-	self:EmitSound("vanilla/turbolaser/vanilla_tl_shot.wav", 511, math.random(95,125));
+	self:EmitSound("heart/turbolaser/heart_turbolaser_shot.wav", 511, math.random(95,125));
 
-	self:PhysicsInit( SOLID_VPHYSICS );
-	self:SetMoveType( MOVETYPE_VPHYSICS );
-	self:SetSolid( SOLID_VPHYSICS );
+	self:PhysicsInit(SOLID_VPHYSICS);
+	self:SetMoveType(MOVETYPE_VPHYSICS);
+	self:SetSolid(SOLID_VPHYSICS);
 
 	local phys = self:GetPhysicsObject();
 
